c931723254da0e2cf5738acf31a47bf95f48e6be
Farok Hossain <farokafs7@gmail.com>
2024-04-24 22:22:57 +0600

    updated

---
+++ src/app/posts/page.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const PostsPage = async () => {
+  const res = await fetch("http://localhost:5000/posts");
+  const posts = await res.json();
+  //   console.log(posts);
+  return (
+    <div className="w-full">
+      <h1 className="text-3xl">Total Post: {posts.length} </h1>
+      {posts.map((post) => (
+        <div
+          key={post.id}
+          className="card w-[70%] my-5 mx-auto bg-base-100 shadow-xl"
+        >
+          <div className="card-body">
+            <h2 className="card-title">{post.title}</h2>
+            <p>{post.description}</p>
+            <p>{post.likes_count}</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">See More</button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PostsPage;
