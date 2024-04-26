5f2a47965727d812bfa61e8a03f36c3d8b0bfaf4
Farok Hossain <farokafs7@gmail.com>
2024-04-27 03:06:46 +0600

    updated

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,6 +1,29 @@
+import Link from "next/link";
+
+// ssg er maddome 2ta id er data load
+// export async function generateStaticParams() {
+//   return [{ id: "1" }, { id: "2" }];
+// }
+
+// ssg er maddome sob gula id er data load
+// jodi 100 ta data thake ar jodi tar modde theke 10 ta data load kora lage tahole map er badole slice use korte hobe
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:5000/posts");
+  const posts = await res.json();
+
+  const ids = posts.map((post) => {
+    return {
+      id: post.id + "",
+    };
+  });
+  //   console.log(ids);
+  return ids;
+}
+
 const DetailsPage = async ({ params }) => {
   const { id } = params;
   //   console.log(params.id);
+  // ssr er maddome data load
   const res = await fetch(`http://localhost:5000/posts/${id}`);
   const post = await res.json();
   //   console.log(post);
@@ -15,6 +38,12 @@ const DetailsPage = async ({ params }) => {
           <h2 className="card-title">{post.title}</h2>
           <p>{post.description}</p>
           <p>Likes: {post.likes_count}</p>
+
+          <div className="card-actions justify-end">
+            <Link href="/posts">
+              <button className="btn btn-accent">Back</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
